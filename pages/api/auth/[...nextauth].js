52f4d22c7248077a53b96864031d8548be8fb0d1
proav2323 <108835201+proav2323@users.noreply.github.com>
2024-03-02 20:10:42 -0600

    coding

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,56 @@
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import {db} from "../../../lib/PtismClient"
+import CredentialsProvider from "next-auth/providers/credentials";
+import { error } from "console";
+import bcrypt from "bcrypt";
+
+
+export default NextAuth({
+  providers: [
+      CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("invalid credentials");
+        }
+
+        const user = await db.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user?.password) {
+          throw new Error("invalid credentials");
+        }
+
+        const isCorrect = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isCorrect) {
+          throw new Error("wrong password for the user");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(db),
+})
