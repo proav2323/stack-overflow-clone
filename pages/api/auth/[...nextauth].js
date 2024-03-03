78040b2da765488a15c08b207797c4d0f9120e63
proav2323 <108835201+proav2323@users.noreply.github.com>
2024-03-02 20:13:34 -0600

    codig

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,7 @@
 import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
 import {db} from "../../../lib/PtismClient"
 import CredentialsProvider from "next-auth/providers/credentials";
-import { error } from "console";
 import bcrypt from "bcrypt";
 
 
